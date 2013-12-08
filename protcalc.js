ae45a3237dfa3efcac75f2e5d811ebc11516facc
zelnor <Lorenz-Becker@gmx.de>
2013-12-08 17:44:36 +0000

    Rewrote into better single-sequence processing, with eye on multi-sequence. A lot of CSS because Web 2.0 hokum. Misc. fixes. Lots of primary research re: Amino Acids

--- protcalc.js
+++ protcalc.js
@@ -1,3 +1,4 @@
+//replace all document.write with a form-field output or something. document.write is terrible.
 if (!String.prototype.format) {
   String.prototype.format = function() {
     var args = arguments;
@@ -10,8 +11,10 @@ if (!String.prototype.format) {
   };
 }
 
-function isolateSubsequence (text){
-	Seqs = [];
+function isolateSubSequences (text){
+	var header = /^>.?\|*/i;
+	var Seqs=[];
+	var lm = 0;
 	for (var i=0; i<text.length; i++){
 		if (text[i].search(header) != -1){
 			console.log("Matched a header at position ", i, ". Last match at ", lm)
@@ -22,55 +25,128 @@ function isolateSubsequence (text){
 	return Seqs
 };
 
-function translateToProtein (text){
+/*
+function translateToProtein (Seq){
 	var codons=[];
-	for (var i=0; i<text.length; i+3){
+	for (var i=0; i<Seq.length; i+3){
 	
 	};
+	document.write("{0} sequence(s) found.<br>".format(Seqs.length));
 };
+*/
+
+function calculateMass (Seq){
+	/*
+	Letter	Amino Acid		Average Mass	Frequency(%)
+	A		alanine			071.0788		8.25
+	R		arginine		156.10111		5.53
+	N		asparagine		114.04293		4.06
+	D		aspartate		115.02694		5.45
+	C		cystine			103.00919		1.37
+	E		glutamate		129.04259		6.75
+	Q		glutamine		128.05858		3.93
+	G		glycine			057.02146		7.07
+	H		histidine		137.05891		2.27
+	I		isoleucine		113.08406		5.95
+	L		leucine			113.08406		9.66
+	K		lysine			128.09496		5.84
+	M		methionine		131.04049		2.42
+	F		phenylalanine	147.06841		3.86
+	P		proline			097.05276		4.70
+	S		serine			087.03203		6.57
+	T		threonine		101.04768		5.34
+	W		tryptophan		186.07931		1.08
+	Y		tyrosine		163.06333		2.92
+	V		valine			099.06841		6.87
+	U		selenocysteine	150.956363		N/A
+	Source: http://web.expasy.org/findmod/findmod_masses.html#AA
+
+	Uncommon codons
+	B	aspartate/asparagine  
+	Z	glutamate/glutamine	
+	X	any									
+	are handled like ExPASY's comput_pi (http://web.expasy.org/compute_pi/pi_tool-doc.html) and computed from their statistics (http://web.expasy.org/docs/relnotes/relstat.html)
+	aspartate	D	115.02694	5.45
+	asparagine	N	114.04293	4.06
 	
+
+	glutamate	E	129.04259	6.75
+	glutamine	Q	128.05858	3.93
+
+	*  translation stop
+	-  gap of indeterminate length
+	*/
+
+	
+};
+
+function calculateAbsorption (Seq){
+	
+};
+
 function parseInput (form) {
 	//Parse header/FASTA bullshit
-	//normalize linebreaks and make into an array:
 	var header = /^>.?\|*/i;
-	var rawSeqs = form.seqbox.value.replace(/\r\n/g, "\n").split("\n");
-	var Seqs = [];
-	var lm = 0;
+	//normalize linebreaks and make into an array:
+	var rawSeq = form.seqbox.value.replace(/\r\n/g, "\n").split("\n");
+	var Seq = [];
+	if (rawSeq[0].search(header) != -1){
+		Seq.push(rawSeqs.slice(1, rawSeq.length).join())
 
+	};
+	parseSequence(Seq)
+};
+
+function parseSequence (Seq){
 	var sequenceName = "Unnamed Sequence";
-	console.log("Seqs: ", Seqs);
-	document.write("{0} sequence(s) found.<br>".format(Seqs.length));
-	for (var i=0; i<Seqs.length; i++){
-		Seq = Seqs[i]	
-		//Search for non-DNA FASTA characters
-		var rxDNA = /[ACGTNUKSYMWRBDHV-]/gi;
-		var rxProtein =/[EFILMPQZX*-]/gi;
-		var isDNA = Seq.search(rxDNA);
-		var isProtein = Seq.search(rxProtein);
-		var result;
-		var results = ['DNA', 'Protein'];
-		switch(isDNA){
-			case -1:
-				if (isProtein>-1){
-					result = 1
-					console.log("Is both DNA and protein. Reset to protein.");
-				}else{
-					result = 0
-				};
-			default:
+	//Search for non-DNA FASTA characters
+	var rxDNA = /[ACGTNUKSYMWRBDHV-]/gi;
+	var rxProtein =/[EFILMPQZX*-]/gi;
+	var isDNA = Seq.search(rxDNA);
+	var isProtein = Seq.search(rxProtein);
+	var result;
+	var results = ['DNA', 'Protein'];
+	switch(isDNA){
+		case -1:
+			if (isProtein>-1){
 				result = 1
-		};
+				console.log("Is both DNA and protein. Reset to protein.");
+			}else{
+				result = 0
+			};
+		default:
+			result = 1
+	};
 		//debug
 		document.write("Auto-detecting sequence type for {0}: {1}<br>".format(sequenceName, results[result]));
 		//translate if needed
+		if (result>0){
+			Seq = translateToProtein(Seq);
+			};
 		//count number of residues
+		Res = [];
+		for (var i=0; i<Res.length; i++){
+			
+		};
 		//calculate mass
 		//calculate coefficient
 		//absorbances
 			//Wait how do I get absorbances PER sequence
-	};
-		
-};
+};		
+/*
+----
+var conc = document.form.A.value / ( document.form.e.value * document.form.d.value ) * 1000000;
+var mconc = document.form.A.value / ( document.form.e.value * document.form.d.value ) * document.form.M.value;
+document.form.concentration.value = conc;
+document.form.mconcentration.value = mconc;
 
-	
+----
+It has been shown [1c] that it is possible to estimate the molar extinction coefficient of a protein from knowledge of its amino acid composition. From the molar extinction coefficient of tyrosine, tryptophan and cystine (cysteine does not absorb appreciably at wavelengths >260 nm, while cystine does) at a given wavelength, the extinction coefficient of the native protein in water can be computed using the following equation:
 
+E(Prot) = Numb(Tyr)*Ext(Tyr) + Numb(Trp)*Ext(Trp) + Numb(Cystine)*Ext(Cystine)
+where (for proteins in water measured at 280 nm): Ext(Tyr) = 1490, Ext(Trp) = 5500, Ext(Cystine) = 125;
+The absorbance (optical density) can be calculated using the following formula:
+Absorb(Prot) = E(Prot) / Molecular_weight
+http://www.ncbi.nlm.nih.gov/pubmed/8563639?dopt=Abstract
+----   
+*/
